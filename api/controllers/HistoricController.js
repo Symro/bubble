9412f37dec1653b86e10b6a54c21560c2b644e2c
Florent <florent.grandval@live.fr>
2014-04-10 10:20:35 +0200

    Optimisation Requete Controleur Historique

--- api/controllers/HistoricController.js
+++ api/controllers/HistoricController.js
@@ -10,7 +10,7 @@ module.exports = {
 	getHistoric: function( req, res, next ){
 
 		// récupère les url playlist dans lequel il a participé
-		Join.find().populate('user').where({ 'user' : req.param('userId') }).populate('playlist').sort('playlistUrl ASC').exec(function foundUserHistoric(err, historic){
+		Join.find({user:req.param('userId')}).populate('playlist').sort('playlistUrl ASC').exec(function foundUserHistoric(err, historic){
 			var historic;
 			var fullHistoric = historic;
 
