8a883cc7edd64fcbc50c86aa85eb77a910024020
symro <sylvain.mro@gmail.com>
2014-04-21 13:00:56 +0200

    limite de la recherche à 20 tracks

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,7 +7,7 @@ var search = {
 	init:function(options){
 
 		this.params=$.extend(this.defaults,options);
-	
+
 	},
 
 	getSearchEngine:function(engine){
@@ -22,12 +22,12 @@ var search = {
 		// console.log(query);
 
 		// on effectue une requête de recherche si cette requête est > à 4 caractères
-		if (query.q.length >= 3) {
+		if (query.length >= 3) {
 
 			// Execution de la requête selon le moteur de recherche
 			if (this.params.engine=='Soundcloud') {
 				// Recherche Souncloud
-				search.params.searchedSouncloud.call(this, query);				
+				search.params.searchedSouncloud.call(this, query);
 			}else {
 				// Recherche Youtube
 				search.params.searchedYoutube.call(this);
